d24e440a6da4c418b52f56019bcc088e29f711ae
Annie Alves <annie.a.alves@gmail.com>
2023-06-19 12:23:17 -0300

    adicionado pdf

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,12 @@ import typing from './type.js'
 
 const headerName = document.querySelector('.header__title');
 const headerDev = document.querySelector('.header__prof');
+const certificadoWomakers = document.getElementById('certificadoWomakers');
 
 
-
+certificadoWomakers.addEventListener('click', () =>{
+    window.open('../src/pdf/Annie Araújo Alves - Certificado Womakers Code.pdf');
+})
 
 
 typing(headerName, headerDev);
