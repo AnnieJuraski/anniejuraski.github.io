47ccd4b6cdc6ecd8516a21fc7887ced43c7c7979
Annie Alves <annie.a.alves@gmail.com>
2023-06-20 00:07:39 -0300

    Adicionado cv, botão scroll to top e projetos

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,27 @@ import typing from './type.js'
 const headerName = document.querySelector('.header__title');
 const headerDev = document.querySelector('.header__prof');
 const certificadoWomakers = document.getElementById('certificadoWomakers');
-
-
-certificadoWomakers.addEventListener('click', () =>{
-    window.open('../src/pdf/Annie Araújo Alves - Certificado Womakers Code.pdf');
-})
+const scrollToTopBTN = document.getElementById('go-up');
 
 
 typing(headerName, headerDev);
 
 
 
+certificadoWomakers.addEventListener('click', () =>{
+    window.open('../src/pdf/Annie Araújo Alves - Certificado Womakers Code.pdf');
+})
+
+window.addEventListener("scroll", function() {    
+    if (window.scrollY > 500) {
+     scrollToTopBTN.classList.add("active");
+    } else {
+     scrollToTopBTN.classList.remove("active");
+    }
+  });
 
 
-
+ scrollToTopBTN.addEventListener("click", ()=> {
+   window.scrollTo(0,0);
+  });
+  
