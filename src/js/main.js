3eb46b22b2abdc619ef789c24d1c73ddba3b8dc9
Annie Alves <annie.a.alves@gmail.com>
2023-06-20 01:03:58 -0300

    Melhorado efeito scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,33 @@
 import typing from './type.js'
+import {showToTopBTN, setActiveLink} from './scroll.js'
 
 const headerName = document.querySelector('.header__title');
 const headerDev = document.querySelector('.header__prof');
 const certificadoWomakers = document.getElementById('certificadoWomakers');
-const scrollToTopBTN = document.getElementById('go-up');
+
 
 
 typing(headerName, headerDev);
+window.addEventListener('scroll', setActiveLink);
+window.addEventListener("scroll", showToTopBTN)
 
 
 
-certificadoWomakers.addEventListener('click', () =>{
+certificadoWomakers.addEventListener('click', () => {
     window.open('../src/pdf/Annie Araújo Alves - Certificado Womakers Code.pdf');
 })
 
-window.addEventListener("scroll", function() {    
-    if (window.scrollY > 500) {
-     scrollToTopBTN.classList.add("active");
-    } else {
-     scrollToTopBTN.classList.remove("active");
-    }
-  });
+
+    
 
 
- scrollToTopBTN.addEventListener("click", ()=> {
-   window.scrollTo(0,0);
-  });
-  
+
+scrollToTopBTN.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
+
+
+
+
+
+
