806aa65af3222dd2eff65bc342cb2a65c03bb68d
Annie Araújo <annie.a.alves@gmail.com>
2023-12-20 17:19:55 -0300

    Atualização

--- src/js/type.js
+++ src/js/type.js
@@ -1,29 +1,29 @@
 export default function typing(element1, element2) {
 
   const nome = "Oi, eu sou a Annie"
-  const titulo = "Desenvolvedora Front-End"
+  const titulo = "Desenvolvedora .Net/C#"
 
 
 
-    const arrText1 = nome.split('');
-    element1.innerHTML = '';
-  
-    const arrText2 = titulo.split('');
-    element2.innerHTML = '';
-  
-    arrText1.forEach((letter, i) => {
-      setTimeout(() => {
-        setTimeout(() => {
-          element1.innerHTML += letter;
-        }, 50 * i);
-      }, 500);
-    });
-  
+  const arrText1 = nome.split('');
+  element1.innerHTML = '';
+
+  const arrText2 = titulo.split('');
+  element2.innerHTML = '';
+
+  arrText1.forEach((letter, i) => {
     setTimeout(() => {
-      arrText2.forEach((letter, i) => {
-        setTimeout(() => {
-          element2.innerHTML += letter;
-        }, 50 * i);
-      });
-    }, 1700); 
-  }
+      setTimeout(() => {
+        element1.innerHTML += letter;
+      }, 50 * i);
+    }, 500);
+  });
+
+  setTimeout(() => {
+    arrText2.forEach((letter, i) => {
+      setTimeout(() => {
+        element2.innerHTML += letter;
+      }, 50 * i);
+    });
+  }, 1700);
+}
